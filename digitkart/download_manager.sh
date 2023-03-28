4a96784ec8e03d964ca68b0b4df173615c2442d0
Nithesh Singh <nithesh.sanjay@infovision.net>
2023-03-28 13:37:05 -0500

    files urls update

---
+++ digitkart/download_manager.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Application directory
+dir_path="/home/setup"
+if [ ! -d $dir_path ]
+then
+echo CRITICAL ERROR: $dir_path does not exist
+exit
+fi
+
+serial=$(cat /proc/device-tree/serial-number)
+echo -e "SERIAL-NUMBER="$serial > $dir_path/fedge-variables.env
+
+echo Downloading configuration files...
+wget https://raw.githubusercontent.com/meontechno/edgedepyml/main/digitkart/device_config.py -O $dir_path/device_config.py
+wget https://raw.githubusercontent.com/meontechno/edgedepyml/main/digitkart/dkart-docker-compose.yml -O $dir_path/docker-compose.yml
--- digitkart/install.sh
+++ digitkart/install.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
-echo Checking for root privileges... 
-if [ "$(id -u)" -ne 0 ]; then echo "Please run as root." >&2; exit 1; fi
+echo Checking for root privileges 
+if [ "$(id -u)" -ne 0 ]; then echo "You should have sudo privilege to run this script. Exiting..." >&2; exit 1; fi
+
+echo Checking for updates... 
+sudo apt-get update
+
+echo Installing python package installer...
+sudo apt-get -y install python3-pip
+
+echo Installing docker-compose...
+pip3 install requests==2.26.*
+pip3 install httpx
+pip3 install docker-compose
 
 echo Updating default runtime to nvidia...
 wget https://raw.githubusercontent.com/meontechno/edgedepyml/main/digitkart/daemon.json -O /etc/docker/daemon.json
 systemctl daemon-reload
 systemctl restart docker
 
-echo Installing docker-compose dependencies...
-pip3 install docker-compose
-
+# Application directory
 dir_path="/home/setup"
 if [ ! -d $dir_path ]
 then
@@ -18,7 +27,7 @@ mkdir $dir_path
 fi
 chmod 777 $dir_path
 
-echo Downloading download-manager... 
+echo Downloading download_manager... 
 wget https://raw.githubusercontent.com/meontechno/edgedepyml/main/digitkart/download_manager.sh -O $dir_path/download_manager.sh
 chmod 777 $dir_path/download_manager.sh
 
@@ -33,3 +42,14 @@ systemctl daemon-reload
 systemctl enable download_manager.service
 systemctl enable device_config.service
 systemctl enable vision_ai.service
+
+echo Configuring docker for non-root user
+if [ "$(grep docker /etc/group)" ]
+then
+    echo docker group already exists
+else
+    groupadd docker
+fi
+usermod -aG docker $USER
+exit
+# newgrp docker #(restart to activate the changes)
