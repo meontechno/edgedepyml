3b4f11a41331546dc00a974793a7bd56c5ef216a
Nithesh Singh <nithesh.sanjay@infovision.net>
2023-03-28 15:32:30 -0500

    Exception handling

--- digitkart/device_config.py
+++ digitkart/device_config.py
@@ -1,21 +1,40 @@
 import os
 import httpx
 
-# string to search in file
-with open(r'/home/setup/fedge-variables.env', 'r') as fp:
-    lines = fp.readlines()
-    for row in lines:
-        if row.find("SERIAL-NUMBER") != -1:
-            serial_num  = row.split("=")[-1]
-fp.close()
 
-header = {"serial-number": serial_num}
-add_items_resp = httpx.get("https://dev.digitkart.ai/api/frictionless/api/kc/getEdgeDeviceConfig", headers=header, timeout=None)
-response_json = add_items_resp.json()
-tenant_id = response_json['data']['X_Tenant_id']
-edge_id = response_json['data']['kcedgedevice_id']
+def get_device_serial():
+    serial = None
+    # string to search in file
+    try:
+        with open(r'/home/setup/fedge-variables.env', 'r') as fp:
+            lines = fp.readlines()
+            for row in lines:
+                if row.find("SERIAL_NUMBER") != -1:
+                    serial = row.split("=")[-1]
+    except Exception as e:
+        print(f"Error occured: {e}\nCannot open env file")
+    return serial
 
-with open("/home/setup/fedge-variables.env", "a") as env_file:
-    env_file.write(f"TENANT_ID={tenant_id}\n")
-    env_file.write(f"EDGE_ID={edge_id}\n")
-env_file.close()
+
+def fetch_config(serial_num):
+    tenant_id, edge_id = None, None
+    header = {"serial-number": serial_num}
+    try:
+        res = httpx.get(dev_config_api, headers=header, timeout=None)
+        res_json = res.json()
+        tenant_id = res_json['data']['X_Tenant_id']
+        edge_id = res_json['data']['kcedgedevice_id']
+    except Exception as e:
+        print(f"Error occured: {e}\nAPI failed!")
+    return tenant_id, edge_id
+
+
+if __name__ == "__main__":
+    dev_config_api = "https://dev.digitkart.ai/api/frictionless/api/kc/getEdgeDeviceConfig"
+    tenant_id, edge_id = fetch_config(get_device_serial())
+    try:
+        with open("/home/setup/fedge-variables.env", "a") as env_file:
+            env_file.write(f"TENANT_ID={tenant_id}\n")
+            env_file.write(f"EDGE_ID={edge_id}\n")
+    except Exception as e:
+        print(f"Error occured: {e}\nCannot create env variables.")
